a4a1a4fc446edd7099fdcce4c6e2ef4ad04f1583
Mille Jørgsholm Bierrings <mill1197@stud.kea.dk>
2021-02-18 14:14:57 +0100

    first version of color palette - no returning values

--- colorSelector.js
+++ colorSelector.js
@@ -1,62 +1,78 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", getValue);
+document.addEventListener("DOMContentLoaded", startColorSelector);
 
-function getValue() {
-  console.log("getValue");
+function startColorSelector() {
+  console.log("Start function");
 
-  colorSelector.addEventListener("input", colorTheBox);
+  document
+    .querySelector("#choose_color")
+    .addEventListener("input", getColorInput);
+
+  getColorInput();
 }
 
-function colorTheBox(event) {
-  document.querySelector(".colorBox").style.backgroundColor =
-    event.target.value;
+function getColorInput() {
+  console.log("Read color input and put into hex");
 
-  let hexValue = event.target.value;
-  console.log(hexValue);
+  let colorValue = document.querySelector("#choose_color").value;
 
-  showHEX(hexValue);
-  hexToRGB(hexValue);
+  const hex = document.querySelector("#hex_color");
+  hex.textContent = "Hex: " + colorValue;
+
+  console.log("HEX: " + colorValue);
+
+  showColor(colorValue);
+  hexToRgb(colorValue);
 }
 
-function showHEX(hexValue) {
-  Number.parseInt(hexValue);
-  document.querySelector(".hex").textContent = `HEX: ${hexValue}`;
+function showColor(colorValue) {
+  document.querySelector("#color_box").style.backgroundColor = colorValue;
 }
 
-function hexToRGB(hexValue) {
-  const r = Number.parseInt(hexValue.substring(1, 3), 16);
-  const g = Number.parseInt(hexValue.substring(3, 5), 16);
-  const b = Number.parseInt(hexValue.substring(5, 7), 16);
+function hexToRgb(colorValue) {
+  console.log("Convert Hex to Rgb and put into rgb");
 
-  console.log(r);
-  console.log(g);
-  console.log(b);
-  showRGB(r, g, b);
+  // Splitter den seks cifrede hex kode op i tre værdier:
+  const r = colorValue.substring(1, 3);
+  const g = colorValue.substring(3, 5);
+  const b = colorValue.substring(5, 7);
+
+  // Udregning af værdier til rgb
+  const r2 = Number.parseInt(r, 16);
+  const g2 = Number.parseInt(g, 16);
+  const b2 = Number.parseInt(b, 16);
+  const rgbResult = `(${r2}, ${g2}, ${b2})`;
+
+  console.log("RGB: " + rgbResult);
+
+  // Sætter RGB farven ind i output
+  const rgb = document.querySelector("#rgb_color");
+  rgb.textContent = "RGB: " + rgbResult;
+
+  rgbToHsl(r2, g2, b2);
 }
 
-function showRGB(r, g, b) {
-  document.querySelector(".rgb").textContent = `RGB: ${r}, ${g}, ${b}`;
-  rgbToHSL(r, g, b);
-}
+function rgbToHsl(r2, g2, b2) {
+  console.log("Convert rgb to hsl and put into hsl");
 
-function rgbToHSL(r, g, b) {
-  r /= 255;
-  g /= 255;
-  b /= 255;
+  r2 /= 255;
+  g2 /= 255;
+  b2 /= 255;
+
   let h, s, l;
 
-  const min = Math.min(r, g, b);
-  const max = Math.max(r, g, b);
+  const min = Math.min(r2, g2, b2);
+  const max = Math.max(r2, g2, b2);
 
   if (max === min) {
     h = 0;
-  } else if (max === r) {
-    h = 60 * (0 + (g - b) / (max - min));
-  } else if (max === g) {
-    h = 60 * (2 + (b - r) / (max - min));
-  } else if (max === b) {
-    h = 60 * (4 + (r - g) / (max - min));
+  } else if (max === r2) {
+    h = 60 * (0 + (g2 - b2) / (max - min));
+  } else if (max === g2) {
+    h = 60 * (2 + (b2 - r2) / (max - min));
+  } else if (max === b2) {
+    h = 60 * (4 + (r2 - g2) / (max - min));
   }
 
   if (h < 0) {
@@ -74,12 +90,14 @@ function rgbToHSL(r, g, b) {
   s *= 100;
   l *= 100;
 
-  //   console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
-  showHSL(h, s, l);
-}
+  // Lav HSL til maks tre cifre
+  h = +h.toFixed(2);
+  s = +s.toFixed(2);
+  l = +l.toFixed(2);
 
-function showHSL(h, s, l) {
-  document.querySelector(".hsl").textContent = `HSL: ${h.toFixed(
-    0
-  )}, ${s.toFixed(0)}%, ${l.toFixed(0)}%`;
+  console.log("hsl(%f,%f%,%f%)", h, s, l);
+
+  // Sætter HSL farven ind i output
+  const hsl = document.querySelector("#hsl_color");
+  hsl.textContent = "HSL: " + h + " " + s + "%" + " " + l + "%";
 }
